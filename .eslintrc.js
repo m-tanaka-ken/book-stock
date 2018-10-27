986cf72d3be39a6c24717fc8411508045ab47229
mtanaka <masaki.ken.yamada@googlemail.com>
2018-10-28 03:13:18 +0900

    ESLint設定見直し

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  parser: 'vue-eslint-parser',
+  root: true,
   parserOptions: {
     parser: 'typescript-eslint-parser'
   },
@@ -9,13 +9,23 @@ module.exports = {
     node: true
   },
   extends: [
-    'eslint:recommended',
-    'typescript',
+    'standard',
     'plugin:vue/recommended',
-    'plugin:prettier/recommended',
     'prettier'
   ],
+  plugins: ['prettier', 'vue', 'typescript'],
   rules: {
-      'prettier/prettier': ['error', { 'singleQuote': true, 'semi': false }]
+    // https://github.com/eslint/typescript-eslint-parser#known-issues
+    'no-undef': 'off',
+    'no-unused-vars': 'off',
+    // https://github.com/eslint/typescript-eslint-parser/issues/445
+    // 'typescript/no-unused-vars': 'error',
+    // https://github.com/vuejs/vue-cli/issues/1672
+    'space-infix-ops': 'off',
+    // temporary fix for https://github.com/vuejs/vue-cli/issues/1922
+    // very strange as somehow this rule gets different behaviors depending
+    // on the presence of typescript-eslint-parser...
+    'strict': 'off',
+    'prettier/prettier': ['error', { 'singleQuote': true, 'semi': false }]
     }
 };
