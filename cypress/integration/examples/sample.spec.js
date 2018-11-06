bf79e277c49998fb4353faa2a33dcb0ea4f6d6c3
mtanaka <masaki.ken.yamada@googlemail.com>
2018-11-07 01:04:31 +0900

    e2eサンプル追加

--- cypress/integration/examples/sample.spec.js
+++ cypress/integration/examples/sample.spec.js
@@ -1,7 +1,8 @@
 describe('サンプル', () => {
   it('表示', () => {
-    cy.visit('http://0.0.0.0:8080/login');
+    cy.visit('http://localhost:8080/login');
     cy.get('input[name=email]').type('sample@sample.sample');
     cy.get('input[name=password]').type('password{enter}');
+    cy.url().should('include', '/');
   });
 });
