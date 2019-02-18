071fa07f042cfc20a79231141d2e7eac3a95e5a6
m-tanaka-ken <masaki.ken.yamada@googlemail.com>
2019-02-19 01:12:26 +0900

    format

--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,18 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  const users = sequelize.define('users', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    token: DataTypes.STRING
-  }, {
-    underscored: true,
-  });
+  const users = sequelize.define(
+    'users',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      token: DataTypes.STRING
+    },
+    {
+      underscored: true
+    }
+  );
   users.associate = function(models) {
     // associations can be defined here
   };
