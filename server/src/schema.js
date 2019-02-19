13cc2c54b12406e02284162fcff30426d53b11d7
m-tanaka-ken <masaki.ken.yamada@googlemail.com>
2019-02-19 23:52:53 +0900

    schema構築

--- server/src/schema.js
+++ server/src/schema.js
@@ -2,7 +2,41 @@ const { gql } = require('apollo-server');
 
 const typeDefs = gql`
   type Query {
-    sample: String!
+    books: [Book]
+    book(id: ID!): Book
+    me: User!
+  }
+
+  type Mutations {
+    createBookById(id: ID): BookCreateResponse
+    updateBookById(id: ID, status: Number): BookUpdateResponse
+    login(email: String): String
+  }
+
+  type Book {
+    id: Number
+    url: String
+    name: String
+    created_at: String
+    borrowed_at: String
+    returned_at: String
+    last_borrowed_user: String
+  }
+
+  type BookCreateResponse {
+    success: Boolean!
+    book: Book
+  }
+
+  type BookUpdateResponse {
+    success: Boolean!
+    book: Book
+  }
+
+  type User {
+    name: String
+    email: String
+    token: String
   }
 `;
 
